f440226bae1606d75af106c6ff9d428dfad60f99
lemonholic <lemonwater@postech.ac.kr>
2023-05-20 21:55:29 +0900

    Delete unnecessary file

--- scripts/run.sh
+++
@@ -1,1 +0,0 @@
-python scripts/txt2img.py --prompt "a professional photograph of an astronaut riding a horse" --ckpt checkpoints/v2-1_512-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml
